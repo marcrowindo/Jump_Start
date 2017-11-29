10cbeb3f29dae62ba024a6765c5f52f04dd80867
Marc Wyss <wyss.a.marc@gmail.com>
2017-11-29 13:31:33 +0100

    Java Script added

--- resources/js/main.js
+++ resources/js/main.js
@@ -7,11 +7,19 @@ $(document).ready(function(){
     draggable: false,
     pauseOnFocus: false,
     pauseOnHover: false
-  });
+	});
 
+  var userScroll = 0;
+  $(window).scroll( function() {
+  	var scrollTop = $(this).scrollTop();
+  	if (scrollTop - userScroll > 50) { 
+  		var navHeight = $('.navbar').css('height');	
+      	$('.navbar').animate({top: '-' + navHeight}, 150);
+      	userScroll = scrollTop; 
+      } else if (userScroll - scrollTop > 50) {
+      	$('.navbar').animate({top: '0px'}, 150);
+      	userScroll = scrollTop;
+      }
+	});
 
-
-
-
-
-
+});
